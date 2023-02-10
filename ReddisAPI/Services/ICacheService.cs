654e6cc98ac2a1422aa3faff1b0ebc6b36467ac9
Simuzeche Kaluwa <simuzechek@gmail.com>
2023-02-10 09:45:01 +0200

    Added Caching Interface

---
+++ ReddisAPI/Services/ICacheService.cs
@@ -0,0 +1,9 @@
+﻿namespace ReddisAPI.Services
+{
+    public interface ICacheService
+    {
+        T GetData<T>(string key);   
+        bool SetData<T>(string key , T data , DateTimeOffset expiration);
+        object RemoveData(string key);
+    }
+}
