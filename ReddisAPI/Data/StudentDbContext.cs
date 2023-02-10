6396b7dfecb806f5449c2e055cb2859870060c55
Simuzeche Kaluwa <simuzechek@gmail.com>
2023-02-10 09:32:52 +0200

    Added Database Context

---
+++ ReddisAPI/Data/StudentDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReddisAPI.Models;
+
+namespace ReddisAPI.Data
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
