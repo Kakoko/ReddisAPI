f7cb7119f2827ff477b180a2914f95aeff8ad685
Simuzeche Kaluwa <simuzechek@gmail.com>
2023-02-10 09:30:20 +0200

    Added Student Model

---
+++ ReddisAPI/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace ReddisAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string RegistrationNumber { get; set; }
+    }
+}
